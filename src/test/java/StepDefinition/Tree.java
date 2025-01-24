acc2e98af48ce0ab40c395b98d33806d7122c2cd
Reshmi <r.reshmi87@gmail.com>
2025-01-23 19:18:00 -0800

    Added Tree features and Tree Step Definition

--- src/test/java/StepDefinition/Tree.java
+++ src/test/java/StepDefinition/Tree.java
@@ -1,5 +1,524 @@
 package StepDefinition;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class Tree {
+	@Given("User is in the Home page after logging in")
+	public void user_is_in_the_home_page_after_logging_in() {
+	    
+	    
+	}
+
+	@When("User clicks on Get Started button under the section Tree")
+	public void user_clicks_on_get_started_button_under_the_section_tree() {
+	    
+	    
+	}
+
+	@Then("User lands on the Tree page")
+	public void user_lands_on_the_tree_page() {
+	    
+	    
+	}
+
+	@Given("User is in the Tree page")
+	public void user_is_in_the_tree_page() {
+	    
+	    
+	}
+
+	@When("User clicks on the hyperlink Overview of Trees")
+	public void user_clicks_on_the_hyperlink_overview_of_trees() {
+	    
+	    
+	}
+
+	@Then("User lands on the Overview of Trees page")
+	public void user_lands_on_the_overview_of_trees_page() {
+	    
+	    
+	}
+
+	@Given("User is in the Overview of Trees page")
+	public void user_is_in_the_overview_of_trees_page() {
+	    
+	    
+	}
+
+	@When("User validates the spacing of the text in the side bar")
+	public void user_validates_the_spacing_of_the_text_in_the_side_bar() {
+	    
+	    
+	}
+
+	@Then("All the texts are aligned and spaced properly.")
+	public void all_the_texts_are_aligned_and_spaced_properly() {
+	    
+	    
+	}
+
+	@When("User clicks on the link Binary Trees from the side bar.")
+	public void user_clicks_on_the_link_binary_trees_from_the_side_bar() {
+	    
+	    
+	}
+
+	@Then("User lands on the Binary Trees page.")
+	public void user_lands_on_the_binary_trees_page() {
+	    
+	    
+	}
+
+	@When("User clicks on the Practice Questions from the side navigation bar")
+	public void user_clicks_on_the_practice_questions_from_the_side_navigation_bar() {
+	    
+	    
+	}
+
+	@Then("User lands on the Practice Questions page and is able to view the content successfully")
+	public void user_lands_on_the_practice_questions_page_and_is_able_to_view_the_content_successfully() {
+	    
+	    
+	}
+
+	@Given("User is in Practice Questions page")
+	public void user_is_in_practice_questions_page() {
+	    
+	    
+	}
+
+	@When("User navigates back to the Overview of Trees window from Practice Question page")
+	public void user_navigates_back_to_the_overview_of_trees_window_from_practice_question_page() {
+	    
+	    
+	}
+
+	@When("User clicks on the Try here>>> button")
+	public void user_clicks_on_the_try_here_button() {
+	    
+	    
+	}
+
+	@Then("User lands on the Try Editor page.")
+	public void user_lands_on_the_try_editor_page() {
+	    
+	    
+	}
+
+	@Given("User is in Try Editor page")
+	public void user_is_in_try_editor_page() {
+	    
+	    
+	}
+
+	@When("User clicks on Run Button without entering the code")
+	public void user_clicks_on_run_button_without_entering_the_code() {
+	    
+	    
+	}
+
+	@Then("An Alert window with the error message is displayed.")
+	public void an_alert_window_with_the_error_message_is_displayed() {
+	    
+	    
+	}
+
+	@When("User clicks on Run Button after entering a invalid python code.")
+	public void user_clicks_on_run_button_after_entering_a_invalid_python_code() {
+	    
+	    
+	}
+
+	@When("User clicks on Run Button after entering a valid python code.")
+	public void user_clicks_on_run_button_after_entering_a_valid_python_code() {
+	    
+	    
+	}
+
+	@Then("Correct output is displayed in the console.")
+	public void correct_output_is_displayed_in_the_console() {
+	    
+	    
+	}
+
+	@When("User navigates back to the Overview of Trees page and then select on Trees from the drop down at the top")
+	public void user_navigates_back_to_the_overview_of_trees_page_and_then_select_on_trees_from_the_drop_down_at_the_top() {
+	    
+	    
+	}
+
+	@Then("User could move back to the Overview of Trees page and then to the Tree page.")
+	public void user_could_move_back_to_the_overview_of_trees_page_and_then_to_the_tree_page() {
+	    
+	    
+	}
+
+	@Given("User is in Tree page")
+	public void user_is_in_tree_page() {
+	    
+	    
+	}
+
+	@When("User clicks on the hyperlink Terminologies.")
+	public void user_clicks_on_the_hyperlink_terminologies() {
+	    
+	    
+	}
+
+	@Then("User lands on the Terminologies page")
+	public void user_lands_on_the_terminologies_page() {
+	    
+	    
+	}
+
+	@Given("User is in Terminologies page")
+	public void user_is_in_terminologies_page() {
+	    
+	    
+	}
+
+	@When("User navigates back to the Terminologies page and then select on Trees from the drop down at the top")
+	public void user_navigates_back_to_the_terminologies_page_and_then_select_on_trees_from_the_drop_down_at_the_top() {
+	    
+	    
+	}
+
+	@Then("User could move back to the Terminologies page and then to the Tree page.")
+	public void user_could_move_back_to_the_terminologies_page_and_then_to_the_tree_page() {
+	    
+	    
+	}
+
+	@When("User clicks on the hyperlink Types of Trees.")
+	public void user_clicks_on_the_hyperlink_types_of_trees() {
+	    
+	    
+	}
+
+	@Then("User lands on the Types of Trees page")
+	public void user_lands_on_the_types_of_trees_page() {
+	    
+	    
+	}
+
+	@Given("User is in Types of Trees page")
+	public void user_is_in_types_of_trees_page() {
+	    
+	    
+	}
+
+	@When("User navigates back to the Types of Trees page and then select on Trees from the drop down at the top")
+	public void user_navigates_back_to_the_types_of_trees_page_and_then_select_on_trees_from_the_drop_down_at_the_top() {
+	    
+	    
+	}
+
+	@Then("User could move back to the Types of Trees page and then to the Tree page.")
+	public void user_could_move_back_to_the_types_of_trees_page_and_then_to_the_tree_page() {
+	    
+	    
+	}
+
+	@When("User clicks on the hyperlink Tree Traversals.")
+	public void user_clicks_on_the_hyperlink_tree_traversals() {
+	    
+	    
+	}
+
+	@Then("User lands on the Tree Traversals page")
+	public void user_lands_on_the_tree_traversals_page() {
+	    
+	    
+	}
+
+	@Given("User is in Tree Traversals page")
+	public void user_is_in_tree_traversals_page() {
+	    
+	    
+	}
+
+	@When("User navigates back to the Tree Traversals page and then select on Trees from the drop down at the top")
+	public void user_navigates_back_to_the_tree_traversals_page_and_then_select_on_trees_from_the_drop_down_at_the_top() {
+	    
+	    
+	}
+
+	@Then("User could move back to the Tree Traversals page and then to the Tree page.")
+	public void user_could_move_back_to_the_tree_traversals_page_and_then_to_the_tree_page() {
+	    
+	    
+	}
+
+	@When("User clicks on the hyperlink Traversals-Illustration.")
+	public void user_clicks_on_the_hyperlink_traversals_illustration() {
+	    
+	    
+	}
+
+	@Then("User lands on the Traversals-Illustration page")
+	public void user_lands_on_the_traversals_illustration_page() {
+	    
+	    
+	}
+
+	@Given("User is in Traversals-Illustration page")
+	public void user_is_in_traversals_illustration_page() {
+	    
+	    
+	}
+
+	@When("User navigates back to the Traversals-Illustration page and then select on Trees from the drop down at the top")
+	public void user_navigates_back_to_the_traversals_illustration_page_and_then_select_on_trees_from_the_drop_down_at_the_top() {
+	    
+	    
+	}
+
+	@Then("User could move back to the Traversals-Illustration page and then to the Tree page.")
+	public void user_could_move_back_to_the_traversals_illustration_page_and_then_to_the_tree_page() {
+	    
+	    
+	}
+
+	@When("User clicks on the hyperlink Binary Trees.")
+	public void user_clicks_on_the_hyperlink_binary_trees() {
+	    
+	    
+	}
+
+	@Then("User lands on the Binary Trees page")
+	public void user_lands_on_binary_trees_page() {
+	    
+	    
+	}
+
+	@Given("User is in Binary Trees page")
+	public void user_is_in_binary_trees_page() {
+	    
+	    
+	}
+
+	@When("User navigates back to the Binary Trees page and then select on Trees from the drop down at the top")
+	public void user_navigates_back_to_the_binary_trees_page_and_then_select_on_trees_from_the_drop_down_at_the_top() {
+	    
+	    
+	}
+
+	@Then("User could move back to the Binary Trees page and then to the Tree page.")
+	public void user_could_move_back_to_the_binary_trees_page_and_then_to_the_tree_page() {
+	    
+	    
+	}
+
+	@When("User clicks on the hyperlink Types of Binary Trees.")
+	public void user_clicks_on_the_hyperlink_types_of_binary_trees() {
+	    
+	    
+	}
+
+	@Then("User lands on the Types of Binary Trees page")
+	public void user_lands_on_the_types_of_binary_trees_page() {
+	    
+	    
+	}
+
+	@Given("User is in Types of Binary Trees page")
+	public void user_is_in_types_of_binary_trees_page() {
+	    
+	    
+	}
+
+	@When("User navigates back to the Types of Binary Trees page and then select on Trees from the drop down at the top")
+	public void user_navigates_back_to_the_types_of_binary_trees_page_and_then_select_on_trees_from_the_drop_down_at_the_top() {
+	    
+	    
+	}
+
+	@Then("User could move back to the Types of Binary Trees page and then to the Tree page.")
+	public void user_could_move_back_to_the_types_of_binary_trees_page_and_then_to_the_tree_page() {
+	    
+	    
+	}
+
+	@When("User clicks on the hyperlink Implementation in Python.")
+	public void user_clicks_on_the_hyperlink_implementation_in_python() {
+	    
+	    
+	}
+
+	@Then("User lands on the Implementation in Python page")
+	public void user_lands_on_the_implementation_in_python_page() {
+	    
+	    
+	}
+
+	@Given("User is in Implementation in Python page")
+	public void user_is_in_implementation_in_python_page() {
+	    
+	    
+	}
+
+	@When("User navigates back to the Implementation in Python page and then select on Trees from the drop down at the top")
+	public void user_navigates_back_to_the_implementation_in_python_page_and_then_select_on_trees_from_the_drop_down_at_the_top() {
+	    
+	    
+	}
+
+	@Then("User could move back to the Implementation in Python page and then to the Tree page.")
+	public void user_could_move_back_to_the_implementation_in_python_page_and_then_to_the_tree_page() {
+	    
+	    
+	}
+
+	@When("User clicks on the hyperlink  Binary Tree Traversals.")
+	public void user_clicks_on_the_hyperlink_binary_tree_traversals() {
+	    
+	    
+	}
+
+	@Then("User lands on the Binary Tree Traversals page")
+	public void user_lands_on_the_binary_tree_traversals_page() {
+	    
+	    
+	}
+
+	@Given("User is in Binary Tree Traversals page")
+	public void user_is_in_binary_tree_traversals_page() {
+	    
+	    
+	}
+
+	@When("User navigates back to the Binary Tree Traversals page and then select on Trees from the drop down at the top")
+	public void user_navigates_back_to_the_binary_tree_traversals_page_and_then_select_on_trees_from_the_drop_down_at_the_top() {
+	    
+	    
+	}
+
+	@Then("User could move back to the Binary Tree Traversals page and then to the Tree page.")
+	public void user_could_move_back_to_the_binary_tree_traversals_page_and_then_to_the_tree_page() {
+	    
+	    
+	}
+
+	@When("User clicks on the hyperlink Implementation of Binary Trees.")
+	public void user_clicks_on_the_hyperlink_implementation_of_binary_trees() {
+	    
+	    
+	}
+
+	@Then("User lands on the Implementation of Binary Trees page")
+	public void user_lands_on_the_implementation_of_binary_trees_page() {
+	    
+	    
+	}
+
+	@Given("User is in Implementation of Binary Trees page")
+	public void user_is_in_implementation_of_binary_trees_page() {
+	    
+	    
+	}
+
+	@When("User navigates back to the Implementation of Binary Trees page and then select on Trees from the drop down at the top")
+	public void user_navigates_back_to_the_implementation_of_binary_trees_page_and_then_select_on_trees_from_the_drop_down_at_the_top() {
+	    
+	    
+	}
+
+	@Then("User could move back to the Implementation of Binary Trees page and then to the Tree page.")
+	public void user_could_move_back_to_the_implementation_of_binary_trees_page_and_then_to_the_tree_page() {
+	    
+	    
+	}
+
+	@When("User clicks on the hyperlink Applications of Binary trees.")
+	public void user_clicks_on_the_hyperlink_applications_of_binary_trees() {
+	    
+	    
+	}
+
+	@Then("User lands on the Applications of Binary trees page")
+	public void user_lands_on_the_applications_of_binary_trees_page() {
+	    
+	    
+	}
+
+	@Given("User is in Applications of Binary trees page")
+	public void user_is_in_applications_of_binary_trees_page() {
+	    
+	    
+	}
+
+	@When("User navigates back to the Applications of Binary trees page and then select on Trees from the drop down at the top")
+	public void user_navigates_back_to_the_applications_of_binary_trees_page_and_then_select_on_trees_from_the_drop_down_at_the_top() {
+	    
+	    
+	}
+
+	@Then("User could move back to the Applications of Binary trees page and then to the Tree page.")
+	public void user_could_move_back_to_the_applications_of_binary_trees_page_and_then_to_the_tree_page() {
+	    
+	    
+	}
+
+	@When("User clicks on the hyperlink Binary Search Trees.")
+	public void user_clicks_on_the_hyperlink_binary_search_trees() {
+	    
+	    
+	}
+
+	@Then("User lands on the Binary Search Trees page")
+	public void user_lands_on_the_binary_search_trees_page() {
+	    
+	    
+	}
+
+	@Given("User is in Binary Search Trees page")
+	public void user_is_in_binary_search_trees_page() {
+	    
+	    
+	}
+
+	@When("User navigates back to the Binary Search Trees page and then select on Trees from the drop down at the top")
+	public void user_navigates_back_to_the_binary_search_trees_page_and_then_select_on_trees_from_the_drop_down_at_the_top() {
+	    
+	    
+	}
+
+	@Then("User could move back to the Binary Search Trees page and then to the Tree page.")
+	public void user_could_move_back_to_the_binary_search_trees_page_and_then_to_the_tree_page() {
+	    
+	    
+	}
+
+	@When("User clicks on the hyperlink Implementation Of BST.")
+	public void user_clicks_on_the_hyperlink_implementation_of_bst() {
+	    
+	    
+	}
+
+	@Then("User lands on the Implementation Of BST page")
+	public void user_lands_on_the_implementation_of_bst_page() {
+	    
+	    
+	}
+
+	@Given("User is in Implementation Of BST page")
+	public void user_is_in_implementation_of_bst_page() {
+	    
+	    
+	}
+
+	@When("User navigates back to the Implementation Of BST page and then select on Trees from the drop down at the top")
+	public void user_navigates_back_to_the_implementation_of_bst_page_and_then_select_on_trees_from_the_drop_down_at_the_top() {
+	    
+	    
+	}
+
+	@Then("User could move back to the Implementation Of BST page and then to the Tree page.")
+	public void user_could_move_back_to_the_implementation_of_bst_page_and_then_to_the_tree_page() {
+	    
+	    
+	}
 
 }
